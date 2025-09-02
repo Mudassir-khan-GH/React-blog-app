3c70dde12ddbf02c5483f45375344ab79f1efd54
Mudassir <mudassirpak47#gmail.com>
2025-09-02 21:20:18 +0500

    redux-toolkit configured

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState,useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import authService from './Appwrite/auth'
+import {login,logout} from './store/authSlice'
 import './App.css'
+import {Header,Footer} from './components/index'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-     <h1>Staryting</h1>
-        
-    </>
-  )
+  const [loading,setLoading] = useState(true)
+
+  const dispatch = useDispatch()
+
+   useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if(userData){
+        dispatch(login({userData}))
+      }else{
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))
+  }, [])
+  
+
+  if (loading) {
+    return <div>Loading...</div>
+  } else {
+    return (
+      <>
+      <div className='bg-gray-700 min-h-screen w-full m-0 p-0'>
+        <div>
+          <Header />
+          <main>
+            // Outlet here 
+            <h1>Outlet</h1>
+          </main>
+          <Footer />
+        </div>
+      </div>
+      </>
+    )
+  }
 }
 
 export default App
